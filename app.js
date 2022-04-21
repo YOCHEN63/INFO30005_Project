4216ac1925e6e8ef6bf05622003a588f41d81c7d
sontang <sontang@student.unimelb.edu.au>
2022-04-21 20:49:42 +1000

    change to connect moogoose

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
-require('dotenv'.config)()
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true})
+require('dotenv/config')
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,useUnifiedTopology: true})
 .then(()=>console.log('MongoDB Connected...'))
 .catch(err=>console.log(err))
 
@@ -22,6 +22,9 @@ app.set('view engine', 'hbs')   // set Handlebars view engine
 // Set up to handle POST requests
 app.use(express.json())     // needed if POST data is in JSON format
 // app.use(express.urlencoded())  // only needed for URL-encoded input
+app.get('/', (req,res) => {
+    res.send('Our demo app is working!')
+})
 app.listen(3000,()=>{
     console.log('App listenting to port 3000')
 })
