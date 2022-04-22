2805177097eb5d0badbffabb8eeac0fb6ea5edf5
sontang <sontang@student.unimelb.edu.au>
2022-04-22 21:35:28 +1000

    router update

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,11 @@ const router = express.Router()
 const schemas = require('../models/User.js')
 
 router.get('/',async(req,res,next)=>{
+    let user = shcemas.user;
+
+    let userResult = await user.find({}).exec((err, userData)=>{
+        if (userData){
+            res.render('index',{data:userData})
+        }
+    })
 })
