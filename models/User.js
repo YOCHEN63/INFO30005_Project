6b68708df49829e349682804d59a743e82494721
sontang <sontang@student.unimelb.edu.au>
2022-04-22 17:39:33 +1000

    update schemas

--- models/User.js
+++ models/User.js
@@ -19,5 +19,31 @@ const UserSchema = new mongoose.Schema({
     }
 })
 
-const UserModel = mongoose.model('User',UserSchema,'users')
-module.exports = UserModel
+const BloodGlucoseSchema = new mongoose.Schema({
+    record_date :{type:Date,default:Date.now},
+    blood_glucose_level : {type:int,required:true}
+})
+
+const ExerciseSchema = new mongoose.Schema({
+    record_date :{type:Date,default:Date.now},
+    walk_steps : {type:int,required:true}
+})
+
+const WeightSchema = new mongoose.Schema({
+    record_date :{type:Date,default:Date.now},
+    weight : {type:int,required:true}
+})
+
+const InsulinSchema = new mongoose.Schema({
+    record_date :{type:Date,default:Date.now},
+    insulin_shots : {type:int,required:true}
+})
+
+const user = mongoose.model('user',UserSchema,'user')
+const exercise = mongoose.model('exercise',ExerciseSchema,'exercise')
+const bloodGlucose = mongoose.model('bloodGlucose',BloodGlucoseSchema,'bloodGlucose')
+const insulin = mongoose.model('insulin',UserSchema,'insulin')
+const weight = mongoose.model('weight',WeightSchema,'weight')
+const myschemas = {'user':user,'exercise':exercise,'bloodGlucose':bloodGlucose
+    ,'insulin':insulin,'weight':weight}
+module.exports = myschemas
