498326bb6e9fd83d4277e2097eef3a27a53b5535
sontang <sontang@student.unimelb.edu.au>
2022-04-25 23:17:17 +1000

    pop out add data page

--- pop.js
+++ pop.js
@@ -1,5 +1,10 @@
 var btn = document.getElementById('add_button');
-var showItem = document.getElementById('comment_popup');
+var showItem = document.getElementById('myForm');
+var closeBtn = document.getElementById('close_button');
 btn.onclick = function show(){
     showItem.style.display = "block";
+}
+
+closeBtn.onclick = function close(){
+    showItem.style.display = "none";
 }
