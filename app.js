115a29492f9a64c49bce954d897eb4fc5e42475d
sontang <sontang@student.unimelb.edu.au>
2022-04-26 17:35:15 +1000

    rearrange htmls into hbs

--- app.js
+++ app.js
@@ -22,8 +22,16 @@ app.set('view engine', 'hbs')   // set Handlebars view engine
 const indexRouter = require('./routes/index')
 // Set up to handle POST requests
 app.use(express.json())     // needed if POST data is in JSON format
+
 // app.use(express.urlencoded())  // only needed for URL-encoded input
+/*
 app.use('/', indexRouter)
+*/
+
+app.get('/', (req,res) => { 
+    res.render('index.hbs') 
+})
+
 app.listen(3000,()=>{
     console.log('App listenting to port 3000')
 })
