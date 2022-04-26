ffc71ebed597561722f4fbba225154d9b0011382
Haoyi Fu <haoyif1@student.unimelb.edu.au>
2022-04-27 08:37:24 +1000

    update: required date save button has alert message for 1.5s delay now

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-/* --------------------------- View Charts Modal --------------------------- */
-var viewChartsModal = document.getElementById("paitent_data_chart_modal");
-var viewChartsBtn = document.getElementById("data_chart_button");
-var closeButn = document.getElementById("modal_return_icon");
-
-viewChartsBtn.onclick = function(){
-    viewChartsModal.style.display = "block";
-}
-
-closeButn.onclick = function(){
-    viewChartsModal.style.display = "none";
-}
-//close the window when click outside of the window
-
-window.onclick = function(event){
-    if(event.target == viewChartsModal){
-        viewChartsModal.style.display = "none";
-    }
+/* -------------------------- Submit Required Data -------------------------- */
+function SubmitRequiredData(){
+    setTimeout(() => { alert("Submit successfully!"); }, 1500);
 }
 
 /* -------------------------- Patient Data Content -------------------------- */
