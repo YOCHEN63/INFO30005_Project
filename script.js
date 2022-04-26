b48bad7a85ddefc0432d567d278ffabf9a9fc69d
Haoyi Fu <haoyif1@student.unimelb.edu.au>
2022-04-26 17:23:29 +1000

    tab bar completed, but the style of Clinician Main is incorrect now

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ window.onclick = function(event){
         viewChartsModal.style.display = "none";
     }
 }
+
+/* -------------------------- Patient Data Content -------------------------- */
+function OpenTable(event, tableName){
+    var i, tabsContent, tabs;
+    tabsContent = document.getElementsByClassName("patient_data_tabs_content");
+
+    for(i = 0; i < tabsContent.length;i++)
+    {
+        tabsContent[i].style.display = "none";
+    }
+
+    tabs = document.getElementsByClassName("patient_data_tabs");
+
+    for(i = 0; i < tabs.length; i++)
+    {
+        tabs[i].className = tabs[i].className.replace(" active","");
+    }
+
+    document.getElementById(tableName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
