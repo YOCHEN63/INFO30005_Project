cbd1f1370b88e721d23abee76dc290968d0f9527
sontang <sontang@student.unimelb.edu.au>
2022-04-27 19:56:38 +1000

    test db with front end

--- app.js
+++ app.js
@@ -25,10 +25,14 @@ app.use(express.json())     // needed if POST data is in JSON format
 
 // app.use(express.urlencoded())  // only needed for URL-encoded input
 
-app.use('/index', indexRouter)
+
 
 app.get('/', (req,res) => { 
     res.render('index.hbs') 
+})
+
+app.post('/', (req,res) => { 
+    app.use('/index', indexRouter)
 })
 
 app.listen(3000,()=>{
