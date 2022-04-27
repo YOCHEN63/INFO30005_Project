35d2b1f26a26003ed1f2e858aded54b25cbb5852
Haoyi Fu <haoyif1@student.unimelb.edu.au>
2022-04-27 22:15:03 +1000

    update: add note is working now and it will appear scroll when notes achieve max height

--- script.js
+++ script.js
@@ -25,21 +25,54 @@ function OpenTable(event, tableName){
 }
 
 /* -------------------------- Patient Note Content -------------------------- */
-var currentDate = new Date();
-var date = currentDate.getDate()+'/'+currentDate.getMonth()+"/"
-           +currentDate.getFullYear();
+function GetDateTime(){
+    var currentDate = new Date();
+    var date = currentDate.getDate()+'/'+currentDate.getMonth()+"/"
+            +currentDate.getFullYear();
 
-var Hours = currentDate.getHours();
-var Minutes = currentDate.getMinutes();
+    var Hours = currentDate.getHours();
+    var Minutes = currentDate.getMinutes();
 
-if(Hours <10){
-    Hours = '0'+Hours;
+    if(Hours <10){
+        Hours = '0'+Hours;
+    }
+
+    if(Minutes <10){
+        Minutes = '0'+Minutes;
+    }
+
+    var time = Hours +':'+ Minutes;
+    var dateTime = date+' '+time;
+
+    return dateTime;
 }
+/* --------------------------- Patient Note Submit -------------------------- */
+var numberOfNote = 0;
 
-if(Minutes <10){
-    Minutes = '0'+Minutes;
+document.querySelector('#clinician_note_submit').onclick = function(numberOfNote){
+    //input can't be none
+    if(document.getElementById("clinician_note_input").value.length == 0){
+        setTimeout(() => { alert("Note message can't be none"); }, 1000);
+    }
+    else{
+        numberOfNote++;
+        var newNote = document.createElement("li");
+        var inputValue = GetDateTime() + " " 
+        + document.getElementById("clinician_note_input").value; 
+        var text = document.createTextNode(inputValue);
+
+        newNote.classList.add("note_message");
+
+        newNote.appendChild(text);
+        newNote.classList.add("single_note_container");
+        document.getElementById("notes").appendChild(newNote);
+
+        //clear input
+        document.getElementById("clinician_note_input").value = '';
+    }
+
+    if(numberOfNote >=4)
+    {
+        
+    }
 }
-
-var time = Hours +':'+ Minutes;
-var dateTime = date+' '+time;
-document.getElementById("note_message_get_time").innerHTML=dateTime;
