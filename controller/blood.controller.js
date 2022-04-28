03d541f78ddef0cbbc87cdbff1da17934ee20f60
sontang <sontang@student.unimelb.edu.au>
2022-04-28 11:06:53 +1000

    minor change to db

--- controller/blood.controller.js
+++ controller/blood.controller.js
@@ -19,50 +19,7 @@ const reqUserData = async (req, res) => {
             return res.sendStatus(404)
         }
         console.log('found userData')
-        return res.render('index',{data:reqBody,bgl_data:bgl_data,exercise_data:exercise_data,insulin_data:insulin_data,weight_data:weight_data})
-    } catch (err) {
-        return next(err)
-    }
-    
-}
-
-/*
-const reqBloodGlucoseData = async (req, res) => {
-    try {
-        const bgl_data = await bloodGlucose.findOne({"_id":'6266f45c3c62e10a62e038f4'}).lean()
-        if (!bgl_data){
-            return res.sendStatus(404)
-        }
-        console.log('found bgl Data')
-        return res.render('index',{bgl_data:bgl_data})
-    } catch (err) {
-        return next(err)
-    }
-    
-}
-
-const reqExerciseData = async (req, res) => {
-    try {
-        const exercise_data = await exercise.findOne({"_id":'6266f45c3c62e10a62e038f4'}).lean()
-        if (!exercise_data){
-            return res.sendStatus(404)
-        }
-        console.log('found exercise data')
-        return res.render('index',{exercise_data:exercise_data})
-    } catch (err) {
-        return next(err)
-    }
-    
-}
-
-const reqInsulinData = async (req, res) => {
-    try {
-        const insulin_data = await insulin.findOne({"_id":'6266f45c3c62e10a62e038f4'}).lean()
-        if (!insulin_data){
-            return res.sendStatus(404)
-        }
-        console.log('found insulin data')
-        return res.render('index',{insulin_data:insulin_data})
+        return res.render('index',{data:reqBody,bgl_data:fixed_bgl,exercise_data:exercise_data,insulin_data:insulin_data,weight_data:weight_data})
     } catch (err) {
         return next(err)
     }
@@ -70,23 +27,14 @@ const reqInsulinData = async (req, res) => {
 }
 
 
-const reqWeightData = async (req, res) => {
-    try {
-        const weight_data = await weight.findOne({"_id":'6266f45c3c62e10a62e038f4'}).lean()
-        if (!weight_data){
-            return res.sendStatus(404)
-        }
-        console.log('found weight data')
-        return res.render('index',{weight_data:weight_data})
-    } catch (err) {
-        return next(err)
-    }
-    
-}
-*/
 const addData = async (req, res) => {
     try {
-        const document = await bloodGlucose(req.body)
+        const document = new bloodGlucose({
+            "user_id":'6266f45c3c62e10a62e038f4',
+            "blood_glucose_level":req.body.blood_glucose_level,
+            "comment":req.body.comment
+        })
+        console.log(req.params.comment)
         await document.save()
         res.redirect('/')
     }catch (err){
