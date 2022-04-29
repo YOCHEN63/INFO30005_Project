7c42f5d52881059e4f049c50974c7cdb013cf95c
YOCHEN <YOCHEN@student.unimelb.edu.au>
2022-04-29 15:42:14 +0800

    threshold update

--- controller/blood.controller.js
+++ controller/blood.controller.js
@@ -176,25 +176,13 @@ const reqDocPatientData = async (req, res) => {
     try {
         /* find data of one specific patient*/
         const onePatient = await myUser.findById(req.params.user_id).lean()
-        var date = onePatient.record_date
-        var bgl_upper = onePatient.bgl_up
-        var bgl_lower = onePatient.bgl_down
-        var weight_upper = onePatient.weight_up
-        var weight_lower = onePatient.weight_down
-        var exercise_upper = onePatient.exercise_up
-        var exercise_lower = onePatient.exercise_down
-        var insulin_upper = onePatient.insulin_up
-        var insulin_lower = onePatient.insulin_down
         var bgl_data = await bloodGlucose.find({"user_id":req.params.user_id}).sort({"record_date": -1}).lean()
         var weight_data = await weight.find({"user_id":req.params.user_id}).sort({"record_date": -1}).lean()
         var exercise_data = await exercise.find({"user_id":req.params.user_id}).sort({"record_date": -1}).lean()
         var insulin_data = await insulin.find({"user_id":req.params.user_id}).sort({"record_date": -1}).lean()
         console.log('doc view data')
-        return res.render('clinician_view_patient',{onePatient:onePatient,bgl_data:bgl_data,exercise_data:exercise_data,insulin_data:insulin_data,weight_data:weight_data,
-                            bgl_lower: bgl_lower, bgl_upper: bgl_upper,
-                            weight_lower: weight_lower, weight_upper: weight_upper,
-                            exercise_lower: exercise_lower, exercise_upper: exercise_upper,
-                            insulin_lower: insulin_lower, insulin_upper: insulin_upper})
+        return res.render('clinician_view_patient',{onePatient:onePatient,
+                        bgl_data:bgl_data,exercise_data:exercise_data,insulin_data:insulin_data,weight_data:weight_data})
     } catch (err) {
         return next(err)
     }
