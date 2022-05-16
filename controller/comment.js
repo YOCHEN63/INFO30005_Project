48629fe6241d20876149a82c636251aa6df4a9b5
yochen <yochen@student.unimelb.edu.au>
2022-05-17 01:06:45 +0800

    request all comment

--- controller/comment.js
+++ controller/comment.js
@@ -1,20 +1,32 @@
 const express = require('express');
-const User = require('../models/User')
+const models = require('../models/User')
+const user = models.user
+const bloodGlucose = models.bloodGlucose
+const exercise = models.exercise
+const insulin = models.insulin
+const weight = models.weight
+
 
 const reqComment = async (req, res, next) => {
-    let bgl_comments = User.bloodGlucose.find().lean()
+    const query = {
+        path : 'user',
+        select : 'clinicianID',
+        match : {clinicianID : req.params.clinician_id}
+    }
+
+    let bgl_comments =  bloodGlucose.find().populate(query).lean()
     for(var bgl_comment in bgl_comments) {
         bgl_comment.dataType = 'blood glucose level'
     }
-    let exercise_comments = User.exercise.find().lean()
+    let exercise_comments =  exercise.find().populate(query).lean()
     for(var exercise_comment in exercise_comments) {
         exercise_comment.dataType = 'exercise'
     }
-    let insulin_comments = User.insulin.find().lean()
+    let insulin_comments =  insulin.find().populate(query).lean()
     for(var insulin_comment in insulin_comments) {
         insulin_comment.dataType = 'insulin'
     }
-    let weight_comments = User.weight.find().lean()
+    let weight_comments =  weight.find().populate(query).lean()
     for(var weight_comment in weight_comments) {
         weight_comment.dataType = 'weight'
     }
@@ -40,4 +52,5 @@ const reqComment = async (req, res, next) => {
  
 }
 
+module.exports = reqComment
 
